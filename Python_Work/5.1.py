51911b1a0936b94814ef4d6489b61632131a0204
keveonaus <119645085+keveonaus@users.noreply.github.com>
2023-07-18 21:57:04 -0500

    5.1 try it yourself

---
+++ Python_Work/5.1.py
@@ -0,0 +1,34 @@
+food = 'pizza'
+print("Is food == 'pizza' ? I predict True.")
+print(food == 'pizza')
+
+print("\nIs food == 'doughnuts' ? I predict False.")
+print(food == 'doughnuts')
+
+football_team = 'packers'
+print("\nIs the football team == 'packers' ? I predict True.")
+print(football_team == 'packers')
+
+print("\nIs the football team == 'bears' ? I predict False.")
+print(football_team == 'bears')
+
+baseball_team = 'brewers'
+print("\nIs the baseball team == 'cubs' ? I predict False.")
+print(baseball_team == 'cubs')
+
+print("\nIs the baseball team == 'brewers' ? I predict True.")
+print(baseball_team == 'brewers')
+
+basketball_team = 'bucks'
+print("\nIs the basketball team == 'bucks' ? I predict True.")
+print(basketball_team == 'bucks')
+
+print("\nIs the basketball team == 'bulls' ? I predict False.")
+print(basketball_team == 'bulls')
+
+soccer_team = 'arsenal'
+print("\nIs the soccer team == 'spurs' ? I predict False.")
+print(soccer_team == 'spurs')
+      
+print("\nIs the soccer team == 'arsenal' ? I predict True.")
+print(soccer_team == 'arsenal')
