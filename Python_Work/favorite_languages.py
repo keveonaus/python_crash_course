584f1b57d534f48df9fc78282749fdd1c3cc5ad9
keveonaus <119645085+keveonaus@users.noreply.github.com>
2023-07-27 21:38:03 -0500

    7/27

---
+++ Python_Work/favorite_languages.py
@@ -0,0 +1,9 @@
+favorite_languages = {
+    'jen': 'pythom',
+    'sarah': 'c',
+    'edward': 'rust',
+    'phil': 'python',
+}
+
+language = favorite_languages['sarah'].title()
+print(f"Sarah's favorite language is {language}.")
