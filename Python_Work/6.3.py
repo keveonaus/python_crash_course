4d3499bfaf346273db0328efb3069ff4900d60f2
keveonaus <119645085+keveonaus@users.noreply.github.com>
2023-08-02 21:47:46 -0500

    8/2 work

---
+++ Python_Work/6.3.py
@@ -0,0 +1,12 @@
+programming_words = {
+        'boolean': 'true or false\n',
+        'python': 'programming language\n',
+        'tuple': 'a list that cannot change\n',
+        'floats': 'a number with a decimal point\n',
+        'strings': 'a series of characters\n',
+        }
+print(programming_words['boolean'])
+print(programming_words['python'])
+print(programming_words['tuple'])
+print(programming_words['floats'])
+print(programming_words['strings'])
--- Python_Work/favorite_languages.py
+++ Python_Work/favorite_languages.py
@@ -1,9 +1,9 @@
 favorite_languages = {
-    'jen': 'pythom',
+    'jen': 'python',
     'sarah': 'c',
     'edward': 'rust',
     'phil': 'python',
     }
 
-language = favorite_languages['sarah'].title()
-print(f"Sarah's favorite language is {language}.")
+for name, language in favorite_languages.items():
+    print(f"{name.title()}'s favorite language is {language.title()}.")
