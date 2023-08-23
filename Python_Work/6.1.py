ac6ec76c5259f3c842e630d916218bb5dc9e9c09
keveonaus <119645085+keveonaus@users.noreply.github.com>
2023-08-22 21:55:32 -0500

    8/22 work

--- Python_Work/6.1.py
+++ Python_Work/6.1.py
@@ -4,3 +4,21 @@ Keven_Ausloos = {
         'age': '26',
         'city': 'milwaukee'
         }
+Brian_Anderson = {
+        'first_name': 'brian',
+        'last_name': 'anderson',
+        'age': '30',
+        'city': 'milwaukee'
+        }
+
+Aaron_Rodgers = {
+        'first_name': 'aaron',
+        'last_name': 'rodgers',
+        'age': '39',
+        'city': 'new york'
+        }
+
+people = [Keven_Ausloos, Brian_Anderson, Aaron_Rodgers]
+
+for people in people:
+    print(people)
