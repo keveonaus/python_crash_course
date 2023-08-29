1a1fe06e64e0ccc7d7e16174f905a5729c4700c3
keveonaus <119645085+keveonaus@users.noreply.github.com>
2023-08-28 21:32:22 -0500

    8/28

--- Python_Work/parrot.py
+++ Python_Work/parrot.py
@@ -1,2 +1,9 @@
-message = input("Tell me somthing, and I will repeat it back to you: ")
-print(message)
+prompt = "\nTell me somthing, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
+
+message = ""
+while message != 'quit':
+    message = input(prompt)
+    
+    if message != 'quit':
+        print(message)
