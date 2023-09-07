5340ebb67a30654299da207cf664a114d7619c2c
keveonaus <119645085+keveonaus@users.noreply.github.com>
2023-09-06 21:08:50 -0500

    Create 7.10.py

---
+++ Python_Work/7.10.py
@@ -0,0 +1,16 @@
+responses = {}
+
+polling_active = True
+while polling_active:
+    name = input("\nWhat is your name? ")
+    response = input("What is your dream vacation? ")
+
+    responses[name] = response
+
+    repeat = input("Would you like another person answer the poll? (yes/no)")
+    if repeat == 'no':
+        polling_active = False
+
+print("Polling Results:")
+for name, response in responses.items():
+    print(f"{name} dream vacation is {response}.")
