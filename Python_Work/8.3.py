226bac33feb7f2dfdb7488cfa0b7bcda3a7f2cce
keveonaus <119645085+keveonaus@users.noreply.github.com>
2023-09-16 15:27:14 -0500

    Create 8.3.py

---
+++ Python_Work/8.3.py
@@ -0,0 +1,6 @@
+def make_shirt(size, message):
+    """Getting size and message"""
+    print(f"\nI want a {size} shirt.")
+    print(f"I want {message} on the shirt.")
+
+make_shirt('medium', message='The Killers')
