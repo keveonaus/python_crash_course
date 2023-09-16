245d490c7d41a53ed078dff052d78251b16b4ba8
keveonaus <119645085+keveonaus@users.noreply.github.com>
2023-09-16 15:29:42 -0500

    Update 8.3.py

--- Python_Work/8.3.py
+++ Python_Work/8.3.py
@@ -1,6 +1,7 @@
-def make_shirt(size, message):
+def make_shirt(message, size='large'):
     """Getting size and message"""
     print(f"\nI want a {size} shirt.")
     print(f"I want {message} on the shirt.")
 
-make_shirt('medium', message='The Killers')
+make_shirt(message='The Killers')
+make_shirt(message='I love python', size='medium')
