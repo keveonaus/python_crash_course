5f4190b1d3775410d7a769ed9f66d605124c0f81
keveonaus <119645085+keveonaus@users.noreply.github.com>
2023-09-16 16:32:54 -0500

    Create 8.6.py

---
+++ Python_Work/8.6.py
@@ -0,0 +1,19 @@
+def get_city_country(city, country):
+    """Returning the city and country"""
+    location = f"{city}, {country}"
+    return location.title()
+
+while True:
+    print("\nGive me a city and the country it is in.")
+    print("(enter 'q' at any time to quit)")
+
+    city = input("City: ")
+    if city == 'q':
+        break
+
+    country = input("Country: ")
+    if country == 'q':
+        break
+
+city_country = get_city_country(city, country)
+print(city_country)
