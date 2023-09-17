ad6703cf05d077e193989f58d852ef53316a5209
keveonaus <119645085+keveonaus@users.noreply.github.com>
2023-09-17 17:45:17 -0500

    Update 8.7.py

--- Python_Work/8.7.py
+++ Python_Work/8.7.py
@@ -5,11 +5,17 @@ def make_album(artist_name, album_title, songs=None):
         album_info['songs'] = songs
     return album_info
 
-album = make_album('the killers', 'hot fuss', songs=12)
-print(album)
+while True:
+    print("\nPlease give me an artists name and an album they made.")
+    print("(enter 'q' to quit)")
 
-album = make_album('avicii', 'true')
-print(album)
+    artist = input("Artist's name: ")
+    if artist == 'q':
+        break
 
-album = make_album('kanye west', 'the college dropout')
-print(album)
+    title = input("A album title they made: ")
+    if title == 'q':
+        break
+
+    album = make_album(artist, title)
+    print(album)
